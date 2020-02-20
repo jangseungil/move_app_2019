88a95f1b095f0c019b949a209525c9dae7e89aff
unknown <csi2@nate.com>
2020-02-21 04:16:27 +0900

    ok

--- src/App.js
+++ src/App.js
@@ -30,14 +30,14 @@ class App extends React.Component {
     console.log("component rendering");
     const { isLoading, movies } = this.state;
     return (
-      <section class="container">
+      <section className="container">
         {isLoading ? 
-          <div class="loader">
-            <span class="loader_text">Loading...</span>
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
           </div>
         
         : (
-          <div class="movies">
+          <div className="movies">
           {movies.map(movie => (
             <Movie 
               key={movie.id} 
@@ -46,6 +46,7 @@ class App extends React.Component {
               title={movie.title} 
               summary={movie.summary} 
               poster={movie.medium_cover_image}
+              genres={movie.genres}
             />
             ))}
           </div>
